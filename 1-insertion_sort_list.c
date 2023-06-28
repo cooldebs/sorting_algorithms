b996f2aabff77a7e73ddbe7e2c02aa642500a2eb
cooldebs <doduronbi83@gmail.com>
2023-06-27 17:50:33 -0700

    Write a function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,48 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * insertion_sort_list - sorts a doubly linked list
- * of integers in ascending order
+ * swap - Swap two nodes in a listint_t doubly-linked list.
+ * @head: pointer to the head of the doubly-linked list.
+ * @n1: pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap(listint_t **head, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*head = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * using the insertion sort algorithm.
+ * @list: doubly-linked list of integers.
  *
- * @list: doubly linked list
- *
- * Return: void
+ * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *tmp;
+	listint_t *current, *ptr, *tmp;
 
-	if (!list)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	current = *list;
-
-	while (current)
+	for (current = (*list)->next; current != NULL; current = tmp)
 	{
-		while (current->next && (current->n > current->next->n))
+		tmp = current->next;
+		ptr = current->prev;
+		while (ptr != NULL && current->n < ptr->n)
 		{
-			tmp = current->next;
-			current->next = tmp->next;
-			tmp->prev = current->prev;
-
-			if (current->prev)
-				current->prev->next = tmp;
-
-			if (tmp->next)
-				tmp->next->prev = current;
-
-			current->prev = tmp;
-			tmp->next = current;
-
-			if (tmp->prev)
-				current = tmp->prev;
-			else
-				*list = tmp;
-
-			print_list(*list);
+			swap(list, &ptr, current);
+			print_list((const listint_t *)*list);
 		}
-		current = current->next;
 	}
 }
